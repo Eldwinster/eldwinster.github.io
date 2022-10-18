b1145d60849ffb18b7081b87dda9be6a3cc55a3e
Eldwinster <yann.arby@gmail.com>
2022-10-19 01:12:27 +0200

    Updating Website files

--- www/ref/files/tryhackme/blue/nmap.sh
+++ www/ref/files/tryhackme/blue/nmap.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env/bash
-ip="10.10.96.212"
+ip="10.10.2.37"
 path=$(pwd)
 timestamp=$(date +"%Y-%m-%dT%H:%M:%S")
 file="${path}"/nmap-report-"${timestamp}".txt
 while :
       do
           if nmap -sn "${ip}" && ping "${ip}" -c 1; then
-              nmap -vvv -T4 -sC -sV -A -p- "${ip}" -oN "${file}"
+              nmap -vvv -T4 -sC -sV -A --script vuln "${ip}" -oX "${file}"
               break
           fi
 done
